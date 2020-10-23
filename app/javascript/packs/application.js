4779797fec16453de6f7e44957a2b8bdeb8e7896
yuki0920 <ywsep20@gmail.com>
2020-10-24 06:16:52 +0900

    Modify default vue files

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,6 +9,13 @@ require("@rails/activestorage").start()
 require("channels")
 //= require materialize
 
+import Vue from 'vue'
+import App from './App.vue'
+
+const app = new Vue({
+  el: '#app',
+  render: h => h(App)
+})
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
