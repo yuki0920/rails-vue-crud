6fa0eed0a892e54422bc1f6e39226c1bd373545f
yuki0920 <ywsep20@gmail.com>
2020-12-25 22:09:20 +0900

    Refactoring

--- app/javascript/store/store.js
+++ app/javascript/store/store.js
@@ -15,14 +15,18 @@ export default new Vuex.Store({
     fetchBooks(state) {
       state.books = [];
       axios.get('/api/books').then(
-        response => {response.data.books.forEach(book => state.books.push(book))}, error => {console.log(error)
-      });
+        response => { response.data.books.forEach(book => state.books.push(book)) },
+        error => { console.log(error) }
+      );
     },
     setBookInfo(state, id){
-      axios.get(`api/books/${id}`).then(response => {
-        state.bookInfo = response.data;
-        state.bookInfoBool = true;
-      });
+      axios.get(`api/books/${id}`).then(
+        response => {
+          state.bookInfo = response.data;
+          state.bookInfoBool = true;
+        },
+        error => { console.log(error) }
+      );
     },
     deleteBook(state, id) {
       axios.delete(`api/books/${id}`).then(
