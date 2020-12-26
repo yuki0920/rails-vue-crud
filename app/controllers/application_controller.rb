e6155175e1d443f07a7914175efba83486251785
yuki0920 <ywsep20@gmail.com>
2020-12-26 09:48:29 +0900

    Add jwt

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  include JWTSessions::RailsAuthorization
+  rescue_from JWTSessions::Errors::Unauthorized, with: :not_authorized
+
+  def current_user
+    @current_user ||= User.find(payload['user_id'])
+  end
+
+  def not_authorized
+    render json: { error: 'Not Authorized' }, status: :unauthorized
+  end
 end
