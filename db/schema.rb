345e0e619ea5bd7bac6fa3e83466f37563faf46f
yuki0920 <ywsep20@gmail.com>
2020-12-26 09:59:32 +0900

    Add user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_23_212015) do
+ActiveRecord::Schema.define(version: 2020_12_26_005534) do
 
   create_table "books", force: :cascade do |t|
     t.string "title", null: false
@@ -21,4 +21,12 @@ ActiveRecord::Schema.define(version: 2020_10_23_212015) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "users", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "email", null: false
+    t.string "password_digest", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+  end
 end
