4e90d33a3b32fe16a1c71a3cb136690804f6cf29
yuki0920 <ywsep20@gmail.com>
2020-12-26 10:01:17 +0900

    Update seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,10 @@
+User.create!(
+  name: 'Admin User',
+  email: 'example@example.com',
+  password: 'foobar',
+  password_confirmation: 'foobar'
+)
+
 20.times do
   Book.create!(
     title: Faker::Book.title,
