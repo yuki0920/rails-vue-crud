5bc8796b9317a8e47fd9b2b24add2d5539306124
yuki0920 <ywsep20@gmail.com>
2020-12-26 10:23:12 +0900

    Add routing

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,9 @@ Rails.application.routes.draw do
 
   namespace :api do
     resources :books, only: %i[index show create update destroy]
+    post 'signup',  controller: :users,    action: :create
+    post 'signin',  controller: :sessions, action: :create
+    delete 'signin', controller: :sessions, action: :destroy
+    post 'refresh', controller: :refresh,  action: :create
   end
 end
